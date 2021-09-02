9efe9a2bcb3524dbf0c1041c354f53e492266a6b
Wesley Westelley <wesleywestelley@gmail.com>
2021-09-02 14:37:05 -0300

    :wrench: feat: connect backend with frontend

--- tinder-frontend/src/components/CardTinder/index.jsx
+++ tinder-frontend/src/components/CardTinder/index.jsx
@@ -1,53 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TinderCard from 'react-tinder-card';
+import axios from '../../axios';
 import './styles.css';
 
 export function CardTinder() {
-  const [people, setpeople] = useState([
-    {
-      name: 'Mônica',
-      url: 'https://images.unsplash.com/photo-1514813482567-2858e6c00ee1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-    },
-    {
-      name: 'Joseph',
-      url: 'https://images.unsplash.com/photo-1496302662116-35cc4f36df92?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-    },
-    {
-      name: 'Rachel',
-      url: 'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80',
-    },
-  ]);
+  const [people, setpeople] = useState();
 
-  const onSwipe = (direction) => {
-    console.log('You swiped: ' + direction);
-  };
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get('/tinder/cards');
+      setpeople(req.data);
+    }
 
-  const onCardLeftScreen = (myIdentifier) => {
-    console.log(myIdentifier + ' left the screen');
-  };
+    fetchData();
+  }, []);
 
   return (
     <section>
-        <div className="tinderCard__cardContainer">
-        {people.map((person) => (
-        <>
-          <TinderCard
-            onSwipe={onSwipe}
-            onCardLeftScreen={() => onCardLeftScreen('fooBar')}
-            preventSwipe={['up', 'down']}
-            className="swipe"
-          >
-            <div
-              style={{ backgroundImage: `url(${person.url})` }}
-              className="card"
+      <div className="tinderCard__cardContainer">
+        {people ? people.map((person) => (
+            <TinderCard
+              preventSwipe={['up', 'down']}
+              className="swipe"
+              key={person.name}
             >
-              <h3>{person.name}</h3>
-            </div>
-          </TinderCard>
-        </>
-      ))}
-        </div>
-      
+              <div
+                style={{ backgroundImage: `url(${person.imgUrl})` }}
+                className="card"
+              >
+                <h3>{person.name}</h3>
+              </div>
+            </TinderCard>
+        )): 'loading...'}
+      </div>
     </section>
   );
 }
